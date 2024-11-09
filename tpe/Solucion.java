5da82cf759161e7bcad0f99dde84a7b27e172481
Jazmin Bascuñan <jazbascunan01@gmail.com>
2024-11-08 23:37:48 -0300

    refactor

--- tpe/Solucion.java
+++ tpe/Solucion.java
@@ -8,79 +8,91 @@ public class Solucion {
     private int tiempoEjecucion;
     private int contadorEstados;
 
+    // Constructor principal que inicializa los procesadores a partir de una lista existente
     public Solucion(ArrayList<Procesador> procesadores) {
-        this.procesadores = new ArrayList<>(procesadores);
-        this.tiempoEjecucion = 0;
-        this.contadorEstados = 0;
+        this();
+        this.procesadores.addAll(procesadores);
     }
 
+    // Constructor vacío que inicializa variables por defecto
     public Solucion() {
-        this.procesadores = new ArrayList<>();
-        this.tiempoEjecucion = 0;
-        this.contadorEstados = 0;
+        procesadores = new ArrayList<>();
+        tiempoEjecucion = 0;
+        contadorEstados = 0;
     }
 
+    // Método que verifica si la solución está vacía
     public boolean isEmpty() {
         return procesadores.isEmpty();
     }
 
+    // Copia la solución actual en una nueva instancia
     public Solucion copiar() {
-        Solucion sol = new Solucion(new ArrayList<>());
-        sol.setTiempoEjecucion(this.tiempoEjecucion);
-        sol.setContadorEstados(this.contadorEstados);
-        for (Procesador p : this.procesadores) {
-            sol.addProcesador(p.copiar());
+        Solucion copia = new Solucion();
+        copia.tiempoEjecucion = this.tiempoEjecucion;
+        copia.contadorEstados = this.contadorEstados;
+
+        // Clona los procesadores en la nueva solución
+        for (Procesador p : procesadores) {
+            copia.addProcesador(p.copiar());
         }
-        return sol;
+        return copia;
     }
 
-    public void actualizarTiempoEjecucion(Procesador p) {
-        if (p.getTiempoEjecucion() > this.tiempoEjecucion) {
-            this.tiempoEjecucion = p.getTiempoEjecucion();
-        }
-    }
-
-    public void actualizarTiempoEjecucion(Procesador p, Tarea t) {
-        if (p.getTiempoEjecucion() == this.tiempoEjecucion) {
-            this.tiempoEjecucion = this.tiempoEjecucion - t.getTiempo();
+    // Actualiza el tiempo de ejecución de la solución si el tiempo del procesador es mayor
+    public void actualizarTiempoEjecucion(Procesador procesador) {
+        if (procesador.getTiempoEjecucion() > tiempoEjecucion) {
+            tiempoEjecucion = procesador.getTiempoEjecucion();
         }
     }
 
+    // Restaura el tiempo de ejecución cuando se desasigna una tarea
+    public void actualizarTiempoEjecucion(Procesador procesador, Tarea tarea) {
+        if (procesador.getTiempoEjecucion() == tiempoEjecucion) {
+            tiempoEjecucion -= tarea.getTiempo();
+        }
+    }
+
+    // Retorna el contador de estados explorados
     public int getContadorEstados() {
         return contadorEstados;
     }
 
+    // Permite modificar el valor del contador de estados
     public void setContadorEstados(int contadorEstados) {
         this.contadorEstados = contadorEstados;
     }
 
+    // Retorna el tiempo de ejecución total
     public int getTiempoEjecucion() {
         return tiempoEjecucion;
     }
 
+    // Asigna un nuevo tiempo de ejecución a la solución
     public void setTiempoEjecucion(int tiempoEjecucion) {
         this.tiempoEjecucion = tiempoEjecucion;
     }
 
+    // Devuelve una copia de la lista de procesadores
     public ArrayList<Procesador> getProcesadores() {
         return new ArrayList<>(procesadores);
     }
 
-    public void addProcesador(Procesador p) {
-        this.procesadores.add(p);
-        if (p.getTiempoEjecucion() > this.tiempoEjecucion) {
-            this.tiempoEjecucion = p.getTiempoEjecucion();
-        }
+    // Agrega un procesador a la solución, actualizando el tiempo de ejecución si es necesario
+    public void addProcesador(Procesador procesador) {
+        procesadores.add(procesador);
+        actualizarTiempoEjecucion(procesador);
     }
 
+    // Incrementa el contador de estados
     public void incrementarContadorEstados() {
-        this.contadorEstados++;
+        contadorEstados++;
     }
 
+    // Genera una representación en texto de la solución
     @Override
     public String toString() {
-        StringBuilder texto = new StringBuilder(
-                """
+        StringBuilder resultado = new StringBuilder("""
                 
                 ========================================
                            Soluci\u00f3n Obtenida           
@@ -88,25 +100,22 @@ public class Solucion {
                 Procesadores involucrados:
                 """);
 
-        if (this.procesadores.isEmpty()) {
-            texto.append("  No se encontraron procesadores.\n");
+        if (procesadores.isEmpty()) {
+            resultado.append("  No se encontraron procesadores.\n");
         } else {
-            for (Procesador p : this.procesadores) {
-                texto.append(p.toString()).append("\n");
+            for (Procesador p : procesadores) {
+                resultado.append(p).append("\n");
             }
         }
 
-        texto.append(String.format("""
+        resultado.append(String.format("""
                                    
                                    Informaci\u00f3n adicional:
                                      -- Tiempo de ejecuci\u00f3n: %d segundos
                                      -- Contador de estados: %d
                                    ****************************************
-                                   """,
-                this.tiempoEjecucion, this.contadorEstados
-        ));
+                                   """, tiempoEjecucion, contadorEstados));
 
-        return texto.toString();
+        return resultado.toString();
     }
-
 }
