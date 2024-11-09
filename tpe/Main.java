3e798a820218aacab49762ec4f4f45c7493af6c6
Jazmin Bascuñan <jazbascunan01@gmail.com>
2024-11-08 22:57:46 -0300

    pruebas de compilacion y soluciones

--- tpe/Main.java
+++ tpe/Main.java
@@ -14,14 +14,22 @@ public class Main {
         /*************************************/
         System.out.println("Tarea 1 DE PRUEBA");
         System.out.println(servicios.servicio1("T1").toString());
+        System.out.println();
+        System.out.println();
         System.out.println("CRITICAS");
         System.out.println(servicios.servicio2(true));
-        System.out.println("PRIORIDAD ENTRE 35 Y 60");
-        System.out.print(servicios.servicio3(35,60).toString());
+        System.out.println();
+        System.out.println();
+        System.out.println("PRIORIDAD ENTRE 30 Y 55");
+        System.out.print(servicios.servicio3(30,55).toString());
+        System.out.println();
+        System.out.println();
         System.out.println("ASIGNAR TAREA 70");
         System.out.println("BACKTRACKING");
         Solucion solucion = servicios.asignarTareasBack(70);
         System.out.println(solucion); 
+        System.out.println();
+        System.out.println();
         System.out.println("GREEDY");
         Solucion solucionGreedy = servicios.asignarTareasGreedy(70);
         System.out.println(solucionGreedy);
